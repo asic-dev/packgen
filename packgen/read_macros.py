f250d4e54e55d527bce18c11a8db296167f1e5c6
asic <asic@ws01-ub1804lts>
2020-05-23 08:43:21 +0200

    implemented looping over macro spec sheets and started to implement extraction of pin table header

--- packgen/read_macros.py
+++ packgen/read_macros.py
@@ -1,5 +1,21 @@
 from xlrd import open_workbook
 
+def extract_pin_table_header(sheet):
+    print("extract pin tbale header")
+    
+    header_dict = {}
+    
+    return(header_dict)
+
+def extract_macro_spec_sheet(sheet):
+    print("extract macro specification for ",sheet.name)
+    
+    print("pin table header:",extract_pin_table_header(sheet))
+
 def read_macros(spec_file):
     
-    wb = open_workbook(spec_file)
+    with open_workbook(spec_file) as wb:
+        print("Read macro specification from ",spec_file)
+        
+        for sheet in wb.sheets():
+            extract_macro_spec_sheet(sheet)
