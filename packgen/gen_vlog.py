99487782792d49be7742a058c2f2934df3205f86
asic <asic@ws01-ub1804lts>
2020-07-01 23:13:46 +0200

    implemented verilog port list generation

--- packgen/gen_vlog.py
+++ packgen/gen_vlog.py
@@ -4,7 +4,17 @@ def gen_vlog(macro):
     
     f = open("{}.v".format(macro.id),"w")
     
-    f.write("module {} () ".format(macro.id))
+    f.write("module {} (\n".format(macro.id))
+
+    seperator = ""
+    for pin in macro.plist:
+        if pin.is_pg_pin():
+            None
+        else:
+            f.write("{}    {}".format(seperator,pin.id))
+            seperator = ",\n"
+
+    f.write("\n);")
     f.write("\n\n")
     
     f.write("endmodule\n\n")
